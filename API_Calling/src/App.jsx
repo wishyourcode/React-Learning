0ea6271012bb1f248ee48ff7cea2e67ceea25f67
wishyourcode <vishalm3101@gmail.com>
2026-01-10 19:51:28 +0530

    learn one for API website and rendering on the UI page

--- API_Calling/src/App.jsx
+++ API_Calling/src/App.jsx
@@ -1,19 +1,41 @@
-import React from "react";
-
+import React, { useState } from "react";
+import axios from "axios";
 const App = () => {
+  const [data, setdata] = useState([]);
   //using fetch method
   const getData = async () => {
     const response = await fetch(
+      //free api or dummy api website json placeholder
       "https://jsonplaceholder.typicode.com/todos/1"
     );
     const data = await response.json();
     console.log(data);
   };
-
+  //using axios
+  const getData2 = async () => {
+    const response = await axios.get(
+      "https://jsonplaceholder.typicode.com/todos/1"
+    );
+    console.log(response.data);
+  };
+  const getData3 = async () => {
+    const response = await axios.get("https://picsum.photos/v2/list");
+    setdata(response.data);
+    console.log(response.data);
+  };
   return (
-    <div>
+    <div className="Apicalling">
       Radhe Radhe <br />
       <button onClick={getData}>GetData</button>
+      <br />
+      <button onClick={getData2}>GetData second Way</button>
+      <br />
+      <button onClick={getData3}>GetData for image</button>
+      <div>
+        {data.map(function (element, idx) {
+          return <h1 key={idx}>Radhe Radhe</h1>;
+        })}
+      </div>
     </div>
   );
 };
