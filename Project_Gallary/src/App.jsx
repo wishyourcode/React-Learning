a1e21f7224617100e3bee34ff73b2bdcff057185
wishyourcode <vishalm3101@gmail.com>
2026-01-11 14:42:51 +0530

    project Completed

--- Project_Gallary/src/App.jsx
+++ Project_Gallary/src/App.jsx
@@ -1,7 +1,44 @@
-import React from "react";
-
+import axios from "axios";
+import { use, useEffect, useState } from "react";
 const App = () => {
-  return <div>Radhe Radhe</div>;
+  const [UserData, setUserData] = useState([]);
+  const getdata = async () => {
+    const response = await axios.get(
+      "https://picsum.photos/v2/list?page=3&limit=20"
+    );
+    setUserData(response.data);
+  };
+  useEffect(function () {
+    getdata();
+  }, []);
+
+  let printUserData = "No Picture Available";
+  if (UserData.length > 0) {
+    printUserData = UserData.map(function (element, idx) {
+      return (
+        <a href={element.url} target="_blank">
+          <div>
+            <div className="h-50 w-50 bg-amber-300 overflow-hidden rounded-2xl">
+              <img
+                key={idx}
+                className="h-[85%] w-full object-center"
+                src={element.download_url}
+                alt=""
+              />
+              <h2 className="text-center">Author: {element.author}</h2>
+            </div>
+          </div>
+        </a>
+      );
+    });
+  }
+  return (
+    <div className="bg-red-300 overflow-auto h-screen">
+      Radhe Radhe
+      <br />
+      <div className="flex flex-wrap gap-4">{printUserData}</div>
+    </div>
+  );
 };
 
 export default App;
