2d2d92a076ae6f2834065c5f6c93d61edcad5da0
wishyourcode <vishalm3101@gmail.com>
2026-01-11 16:39:15 +0530

    Paging implemented

--- Project_Gallary/src/App.jsx
+++ Project_Gallary/src/App.jsx
@@ -2,16 +2,20 @@ import axios from "axios";
 import { use, useEffect, useState } from "react";
 const App = () => {
   const [UserData, setUserData] = useState([]);
+  const [Index, setIndex] = useState(2);
   const getdata = async () => {
     const response = await axios.get(
-      "https://picsum.photos/v2/list?page=3&limit=20"
+      `https://picsum.photos/v2/list?page=${Index}&limit=21`
     );
     setUserData(response.data);
   };
   //automatically called useEffect
-  useEffect(function () {
-    getdata();
-  }, []);
+  useEffect(
+    function () {
+      getdata();
+    },
+    [Index]
+  );
 
   let printUserData = "No Picture Available";
   if (UserData.length > 0) {
@@ -35,9 +39,32 @@ const App = () => {
   }
   return (
     <div className="bg-red-300 overflow-auto h-screen">
-      Radhe Radhe
+      <h1 className="text-center text-2xl text-white bg-amber-900 h-10 flex justify-center">
+        Radhe Radhe
+      </h1>
       <br />
-      <div className="flex flex-wrap gap-4">{printUserData}</div>
+      <div className="flex flex-wrap p-2 gap-4">{printUserData}</div>
+      <div className="flex justify-center gap-6 text-white items-center p-4">
+        <button
+          onClick={() => {
+            if (Index > 1) {
+              setIndex(Index - 1);
+            }
+          }}
+          className="bg-amber-950 cursor-pointer active:scale-90 px-4 py-2 rounded gap-3"
+        >
+          Prev
+        </button>
+        <h2 className="bg-amber-900 p-1 rounded-2xl">{Index}</h2>
+        <button
+          onClick={() => {
+            setIndex(Index + 1);
+          }}
+          className="bg-amber-950 cursor-pointer active:scale-90 px-4 py-2 rounded gap-3"
+        >
+          Next
+        </button>
+      </div>
     </div>
   );
 };
