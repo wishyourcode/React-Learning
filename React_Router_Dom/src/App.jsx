014e2fade3e56f43d5dc0ca5a70cf92968c665a5
wishyourcode <vishalm3101@gmail.com>
2026-01-12 12:00:56 +0530

    react Router dom learned

--- React_Router_Dom/src/App.jsx
+++ React_Router_Dom/src/App.jsx
@@ -1,7 +1,23 @@
-import React from "react";
-
+import { Link, Route, Routes } from "react-router-dom";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Home from "./pages/home";
+import Navbar from "./component/Navbar";
+import Footer from "./component/Footer";
 const App = () => {
-  return <div>Radhe Radhe</div>;
+  return (
+    <div>
+      <Navbar />
+      <Footer />
+      <div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/About" element={<About />} />
+          <Route path="/Contact" element={<Contact />} />
+        </Routes>
+      </div>
+    </div>
+  );
 };
 
 export default App;
---
+++ React_Router_Dom/src/pages/Contact.jsx
@@ -0,0 +1,12 @@
+import Navbar2 from "../component/Navbar2";
+
+const Contact = () => {
+  return (
+    <div>
+      <h1>Contact Page</h1>
+      <Navbar2 />
+    </div>
+  );
+};
+
+export default Contact;
